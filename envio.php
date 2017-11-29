1852910ad09cd6cb8720c3d89ac0d93955020344
Gabriel Lima <mugiwara.gl@gmail.com>
2017-11-29 15:46:44 -0300

    novo layout/ inserindo logo e fundo/ biblioteca Jquery

--- envio.php
+++ envio.php
@@ -1,39 +1,94 @@
+<?php 
+	session_start();
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Enviando email...</title>
 </head>
-<body>
-	<?php 
-	$campus = $_POST['campus'];
-	$nome = $_POST['nome'];
-	$matricula = $_POST['matricula'];
+	<body>
+		<?php 
+		// $coord = '';
 
-	$periodo = $_POST['periodo'];
-	$curso = $_POST['curso'];
-	$turno = $_POST['turno'];
-	$fone = $_POST['fone'];
+			$campus = $_POST['campus'];
+			$nome = $_POST['nome'];
+			$matricula = $_POST['matricula'];
 
-	$cpf = $_POST['cpf'];
-	$rg = $_POST['rg'];
-	$email = $_POST['email'];
+			$periodo = $_POST['periodo'];
+			$curso = $_POST['curso'];
+			// if ($curso == 'ipi') {
+			// 	// email de alexandre
+			// } else if ($curso == 'log') {
+			// 	// email de renata
+			// }
+			$turno = $_POST['turno'];
+			$fone = $_POST['fone'];
 
-	$item = $_POST['itens'];
-	$obs = $_POST['obs'];
-	$file = $file
+			$cpf = $_POST['cpf'];
+			$rg = $_POST['rg'];
+			$email = $_POST['email'];
 
-	 
-		$to = ""; #neste campo será inserido o email de destino do formulario.
-		$subject = "";
-	 	$header = "MIME-version: 1.0\n"; 
-	 	$header = "content-type: text/html; charset=iso-8859-1\n";
-	 	$header = "From: $email\n";
-	 	mail($to,$subject,$message,$header);
-	 	echo "Requerimento enviado com sucesso!!";
+			$item = $_POST['itens'];
+			$outros = $_POST['outros'];
+			$obs = $_POST['obs'];
+			$file = $_FILES['file'];
 
-	 ?>
+		 
+			// $to = "mugiwara.gl@gmail.com"; #neste campo será inserido o email de destino do formulario.
+			$subject = "$item";
+			$message = " - Requerimento do Aluno -
+				Campus : $campus        Nome do Aluno(a): $nome      matricula : $matricula
+					periodo : $periodo   Curso : $curso    Turno : $turno   Telefone : $fone
+				CPF : $cpf    Identidade : $rg   E-mail : $email    
+						Item : $item   Outros : $outros    
+					Observação : $obs  Anexos : $file
+						";
+		 	$header = "MIME-version: 1.0\n"; 
+		 	$header = "content-type: text/html; charset=iso-8859-1\n";
+		 	$header = "From: $email\n";
+		 	mail($to,$subject,$message,$header);
+		 // 	echo "Requerimento enviado com sucesso!!";
 
 
+		 	
+			$to = "mugiwara.gl@gmail.com"; #neste campo será inserido o email de destino do formulario.
 
-</body>
+			$file = isset($_FILES["file"]) ? $_FILES["file"] : FALSE;
+			if(file_exists($file["tmp_name"]) and !empty($file)){
+			$fp = fopen($_FILES["file"]["tmp_name"],"rb");
+			$anexo = fread($fp,filesize($_FILES["file"]["tmp_name"]));
+			$anexo = base64_encode($anexo);
+			fclose($fp);
+			$anexo = chunk_split($anexo);
+			$boundary = "XYZ-" . date("dmYis") . "-ZYX";
+			$mens = "--$boundary\n";
+			$mens .= "Content-Transfer-Encoding: 8bits\n";
+			$mens .= "Content-Type: text/html; charset=\"ISO-8859-1\"\n\n"; //plain
+			$mens .= "$message\n";
+			$mens .= "--$boundary\n";
+			$mens .= "Content-Type: ".$file["type"]."\n";
+			$mens .= "Content-Disposition: attachment; filename=\"".$file["name"]."\"\n";
+			$mens .= "Content-Transfer-Encoding: base64\n\n";
+			$mens .= "$anexo\n";
+			$mens .= "--$boundary--\r\n";
+			$headers = "MIME-Version: 1.0\n";
+			$headers .= "From: \"$email\"\n";
+			$headers .= "Content-type: multipart/mixed; boundary=\"$boundary\"\r\n";
+			$headers .= "$boundary\n";
+			//envio o email com o anexo
+			mail($email,$subject,$mens,$headers);
+			echo"Email enviado com Sucesso!";
+			}
+			//se não tiver anexo
+			else{
+			$headers = "MIME-Version: 1.0\r\n";
+			$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+			$headers .= "From: \"$nome\" <$email_from>\r\n";
+			//envia o email sem anexo
+			mail($to,$subject,$message, $header);
+			echo"Email enviado com Sucesso!";
+	}
+	?>
+
+	</body>
 </html>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+  session_start();
+ ?>
+ 
 <!doctype html>
 <html lang="en">
   <head>
@@ -5,9 +9,23 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="formulario.css">
+    <script type="text/javascript" src="jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="jquery.mask.min.js"></script>
+    <script type="text/javascript" src="jquery.mask.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.0/jquery.mask.js"></script>
+    
+    <script type="text/javascript">
+      $(document).ready(function (){
+        var $Tel = $('#tel');
+        $Tel.mask('(00) 0000-0000');
+      });
+    </script>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+
   </head>
     <body>
       <!-- Optional JavaScript -->
@@ -15,107 +33,129 @@
       <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-
+      
+      <!--cabeçalho  -->
+      <div id="hearder">
+         <div class="barra">
+              <div class="large-12 medium-12 small-12">
+                  <div class="large-2 medium-2 small-2 columns">
+                    <div id="logo-reitoria">
+                      <a class="acesso-reitoria" href="/inscricao/"> <img src="logo-reitoria.png "></a>
+                    </div>
+                  </div>
+                <!--LOGO DA CRADT 
+                  <div class="large-2 medium-2 small-2 columns">
+                    <div id="logo-pronatec"><a class="logo-pronatec"><img src=" AQUI" ></a></div>
+                  </div>  -->                                      
+              </div>              
+         </div>
+    </div>
       <div class="container">
           <form method="POST" action="envio.php">
             <fieldset>
-              <legend>Requerimento - Aluno(a)</legend>
+              <legend style="text-align: left;font-size: 30px"> Requerimento - Aluno(a) </legend>
               <div class="form-group">
                 <table class="table table-sm ">
-                  <thead >
-                      <tr>
-                        <th scope="col">Campus</th>
-                        <th scope="col">Nome do(a) aluno(a)</th>
-                        <th scope="col">Matrícula</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                      <tr>
-                          <td> <label> <input class="form-control" type="text" name="campus"> </label> </td> 
-                          <td> <label> <input class="form-control" type="text" name="nome"> </label> </td>
-                          <td> <label> <input class="form-control" type="text" name="matricula" required> </label> </td>
-                      </tr>
-                  </tbody>
-                      <!-- -------------------- ---------------------- ------------------------ -->
-                  <thead>
-                      <tr>
-                        <th scope="col">Periodo</th>
-                        <th scope="col">Curso</th>
-                        <th scope="col">Turno</th>
-                        <th scope="col">Telefone</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td> <label> <input class="form-control" type="number" min="1" max="4" name="periodo"> </label> </td>
-                      <td> <label> <select class="form-control" name="curso"> 
-                          <option disabled selected> -- </option> 
-                          <option value="ipi"> Informatica para Internet</option>
-                          <option value="log"> Logistica</option>
-                        </select></label>
-                      </td>
-                      <td> <label> <select  class="form-control" name="turno">
-                           <option disabled selected>----</option> 
-                           <option value="Manha">Manhã</option> 
-                           <option value="tarde">Tarde</option>
-                          </select></label>
-                      </td>
-                      <td> <label> <input class="form-control" type="text" name="fone"> </label> </td>
-                    </tr>
-                  </tbody>
-                  <!-- ----------------- ---------------------- ---------------- ------------------ -->
-                  <thead> 
-                    <tr>
-                      <th scope="col">CPF</th>
-                      <th scope="col">Identidade</th>
-                      <th scope="col">Email</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td> <label> <input class="form-control" type="text" name="cpf" required> </label> </td>
-                      <td><label> <input class="form-control" type="text" name="rg" required></label> </td>
-                      <td> <label> <input class="form-control" type="text" name="email"></label> </td>
-                    </tr>
-                  </tbody>
-                  <!-- ------------------- ---------------------- ------------------------ ------------------ -->
-                  <thead>
-                    <tr>
-                      <th scope="col">Itens</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                     <td> <label> <select  class="form-control" name="itens">
-                           <option disabled selected>----</option> 
-                           <option value="cancelamento">Cancelamento de Matrícula</option> 
-                           <option value="declaracao">Declaração de Matrícula ou Vicnculo</option>
-                           <option value="diploma">Diploma</option>
-                           <option value="historico">Histórico Escolar</option>
-                           <option value="falta">Justificativa de Falta</option>
-                           <option value="reabertura">Reabertura de Matrícula</option>
-                           <option value="trancamento">trancamento de Matrícula</option>
-                          </select></label>
-                      </td>
-                       </td>
-                    </tr>
-                  </tbody>
-                  <!-- ------------------------ ----------------------- ----------------- --------------------- -->
-                  <thead>
-                      <tr>
-                        <th scope="col">Observações</th>
-                        <th scope=""> Anexo de arquivos</th>
-                      </tr> 
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td> <label> <textarea class="form-control" rows="5" name="obs"></textarea> </label></td>
-                      <td> <label> <input type="file" class="form-control-file" name="file"></label></td>
-                    </tr>
-                  </tbody>
+                      <thead >
+                          <tr >
+                            <th scope="col">Campus</th>
+                            <th scope="col">Nome do(a) aluno(a)</th>
+                            <th scope="col">Matrícula</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                          <tr>
+                              <td> <label> <input class="form-control" type="text" name="campus"> </label> </td> 
+                              <td> <label> <input class="form-control" type="text" name="nome"> </label> </td>
+                              <td> <label> <input class="form-control" type="text" name="matricula" required> </label> </td>
+                          </tr>
+                      </tbody>
+                          <!-- -------------------- ---------------------- ------------------------ -->
+                      <thead>
+                          <tr>
+                            <th scope="col">Periodo</th>
+                            <th scope="col">Curso</th>
+                            <th scope="col">Turno</th>
+                            <th scope="col">Telefone</th>
+                          </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td> <label> <input class="form-control" type="number" min="1" max="4" name="periodo"> </label> </td>
+                          <td> <label> <select class="form-control" name="curso"> 
+                              <option disabled selected> -- </option> 
+                              <option value="ipi"> Informatica para Internet</option>
+                              <option value="log"> Logistica</option>
+                            </select></label>
+                          </td>
+                          <td> <label> <select  class="form-control" name="turno">
+                               <option disabled selected>----</option> 
+                               <option value="Manha">Manhã</option> 
+                               <option value="tarde">Tarde</option>
+                              </select></label>
+                          </td>
+                          <td> <label> <input class="form-control" id="tel" type="text" name="fone"> </label> </td>
+                        </tr>
+                      </tbody>
+                      <!-- ----------------- ---------------------- ---------------- ------------------ -->
+                      <thead> 
+                        <tr>
+                          <th scope="col">CPF</th>
+                          <th scope="col">Identidade</th>
+                          <th scope="col">Email</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+
+                          <td> <label> <input  class="form-control" type="text" name="cpf" id="cpf" required> </label> </td>
+                          <td><label> <input  class="form-control" type="text" name="rg" id="rg" required></label> </td>
+                          <td> <label> <input  class="form-control" type="text" name="email"></label> </td>
+                        </tr>
+                      </tbody>
+                      <!-- ------------------- ---------------------- ------------------------ ------------------ -->
+                      <thead>
+                        <tr>
+                          <th scope="col">Itens</th>
+                          <th scope="col"> Outros</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                         <td> <label> <select  class="form-control" name="itens">
+                               <option disabled selected>----</option> 
+                               <option value="cancelamento">Cancelamento de Matrícula</option> 
+                               <option value="declaracao">Declaração de Matrícula ou Vinculo</option>
+                               <option value="diploma">Diploma</option>
+                               <option value="historico">Histórico Escolar</option>
+                               <option value="falta">Justificativa de Falta</option>
+                               <option value="reabertura">Reabertura de Matrícula</option>
+                               <option value="trancamento">trancamento de Matrícula</option>
+                              </select></label>
+                          </td>
+                          <td>
+                            <label> <textarea class="form-control" rows="3" name="outros"></textarea></label>
+                          </td>
+                           </td>
+                        </tr>
+                      </tbody>
+                      <!-- ------------------------ ----------------------- ----------------- --------------------- -->
+                      <thead>
+                          <tr>
+                            <th scope="col">Observações</th>
+                            <th scope=""> Anexo de arquivos</th>
+                          </tr> 
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>
+                           <label> <textarea class="form-control" rows="5" name="obs"></textarea> </label></td>
+                          <td> <label> <input type="file" class="form-control-file" name="file"></label></td>
+                        </tr>
+                      </tbody>
                 </table>
                 <br><br>
-                <input type="submit" class="btn btn-primary btn-lg btn-block" value="Enviar">
+                <input type="submit" class="btn btn-lg btn-block" value="Enviar">
               </div>
             </fieldset>
           </form>
